69a5f9ad1b245a3c6ccab73b71aaf15e164072ee
Mellowbun <couch.devan@gmail.com>
2024-01-03 12:57:39 -0800

    Add project files.

---
+++ MyFirstFullStack.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstFullStack.Server.Models;
+
+namespace MyFirstFullStack.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ MyFirstFullStack.Server/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstFullStack.Server.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int PageCount { get; set; }
+    }
+}
